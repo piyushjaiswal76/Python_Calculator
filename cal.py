7219454a2c08e35ec2efeef13d87ccb42a7574a1
Piyush jaiswal <piyushjaiswal602@gmail.com>
2023-10-30 16:06:35 +0530

    add files

---
+++ cal.py
@@ -0,0 +1,56 @@
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Main calculator function
+def calculator():
+    print("Simple Calculator")
+    while True:
+        print("\nOperations:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Quit")
+
+        choice = input("Enter choice (1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Goodbye!")
+            break
+
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print("Result:", add(num1, num2))
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+            elif choice == '4':
+                result = divide(num1, num2)
+                if result == "Cannot divide by zero":
+                    print(result)
+                else:
+                    print("Result:", result)
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    calculator()
